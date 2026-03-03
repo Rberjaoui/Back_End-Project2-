f6ce1a8d639f26ca5ec7e06b848591831ffa3239
Brandon <brandon.z.cano@gmail.com>
2026-03-03 01:54:43 -0800

    Job Application controller

--- src/main/java/com/group7/jobTrackerApplication/controller/JobApplicationController.java
+++ src/main/java/com/group7/jobTrackerApplication/controller/JobApplicationController.java
@@ -1,4 +1,56 @@
 package com.group7.jobTrackerApplication.controller;
 
-public class JobApplicationController {
+import com.group7.jobTrackerApplication.model.jobApplication;
+import com.group7.jobTrackerApplication.service.jobApplicationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/job-applications")
+public class jobApplicationController {
+
+    private final JobApplicationService jobApplicationService;
+
+    public jobApplicationController( JobApplicationService jobApplicationService){
+        this.jobApplicationService = jobApplicationService;
+    }
+
+    @GetMapping
+    public ReponseEntity<List<jobApplication>> getAll(){
+        return ResponseEntity.ok(jobApplicationService.getAll());
+    }
+
+    @GetMapping("/{applicationId}")
+    public ReponseEntity<jobApplication> getById(@PathVariable Long applicationId){
+        return ResponseEntity.ok(jobApplicationService.getById());
+    }
+
+    @PostMapping
+    public ResponseEntity<jobApplication> create(@RequestBody jobApplication jobApplication){
+        jobApplication created = jobApplicationService.create(jobApplication);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{applicationId}")
+    public ResponseEntity<jobApplication> replace(@PathVariable Long applicationId, @RequestBody jobApplication jobApplication ){
+        jobApplication updated = jobApplicationService.replace(applicationId, jobApplication);
+        return ResponseEntity.ok(updated);
+    }
+
+    @PatchMapping("/{applicationId}")
+    public ResponseEntity<jobApplication> patch(@PathVariable Long applicationId, @RequestBody Map<String, Object> updates){
+        jobApplication patched = jobApplicationService.replace(applicationId, updates);
+        return ResponseEntity.ok(patched);
+    }
+
+    @DeleteMapping("/{applicationId}")
+    public ResponseEntity<jobApplication> delete(@PathVariable Long applicationId){
+        jobApplicationService.delete(applicationId);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
